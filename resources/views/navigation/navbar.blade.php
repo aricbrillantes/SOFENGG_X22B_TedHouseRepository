2cbc26111014d8cd0b1cf4a351bc4af709376989
czeideavanzado <czeide_avanzado@dlsu.edu.ph>
2017-11-03 17:27:50 +0800

    fixed navigation bar

--- resources/views/navigation/navbar.blade.php
+++ resources/views/navigation/navbar.blade.php
@@ -1,20 +1,18 @@
 <nav class="navbar navbar-inverse">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">{{ config('app.name', 'LSAPP') }}</a>
-        </div>
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/">{{ config('app.name', 'LSAPP') }}</a>
+    </div>
 
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li><a class="nav-link disabled" href="/">Home</a></li>
-                <li><a href="/about">About us</a></li>
-            </ul>
-        </div>
+    <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+            <li><a class="nav-link disabled" href="/">Home</a></li>
+            <li><a href="/about">About us</a></li>
+        </ul>
     </div>
 </nav>
