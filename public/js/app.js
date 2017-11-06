562086e7d2828e376565c118321f497151433843
czeideavanzado <czeide_avanzado@dlsu.edu.ph>
2017-11-06 10:52:34 +0800

    Added no search results

--- public/js/app.js
+++ public/js/app.js
@@ -42545,9 +42545,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-5eaf12cf", Component.options)
+    hotAPI.createRecord("data-v-b54a4e9c", Component.options)
   } else {
-    hotAPI.reload("data-v-5eaf12cf", Component.options)
+    hotAPI.reload("data-v-b54a4e9c", Component.options)
 ' + '  }
   module.hot.dispose(function (data) {
     disposed = true
@@ -42734,7 +42734,7 @@ module.exports = { render: render, staticRenderFns: staticRenderFns }
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-    require("vue-hot-reload-api")      .rerender("data-v-5eaf12cf", module.exports)
+    require("vue-hot-reload-api")      .rerender("data-v-b54a4e9c", module.exports)
   }
 }
 
--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -6,10 +6,14 @@
         <!-- SEARCH RESULTS -->
         <h1>Search Results for <txt id="key">Really Nice</txt></h1>
 
+        <div align="center" id="no_found">
+				Nothing found for <txt id="key">Really Nice</txt>
+		</div>
+
         <!-- DIV FOR EACH RESULT -->
-        <div id="result">
+        {{--  <div id="result">
             <a href="/" id="s_name">A Really Nice Thesis</a><br>
-            <a href="/" id="s_Author">De la Cruz, Juan</a>
+            <a href="/" id="s_author">De la Cruz, Juan</a>
             <a id="s_date">2010, October 31</a>
             
             <a id="s_finished">FINISHED</a>
@@ -24,19 +28,19 @@
                 <li><a href="" id="a_tag">Thesis</a></li>
                 <li><a href="" id="a_tag">Cool</a></li>
             </ul>
-        </div>
+        </div>  --}}
         
     </div>
 @endsection
 
 @section('bottom')
     <!-- PAGE NAVIGATION -->
-    <div id="bottom">
+    {{--  <div id="bottom">
         <a href="" id="prev">Prev</a>
         <ul id="pages">
             <li><a href="" id="a_page">1</a></li>
             <li><a href="" id="a_page">2</a></li>
         </ul>
         <a href="" id="next">Next </a>
-    </div>
+    </div>  --}}
 @endsection
