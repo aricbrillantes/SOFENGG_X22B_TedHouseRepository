19e79140a7c429115995ed97fa2182a1a5a38ceb
czeideavanzado <czeide_avanzado@dlsu.edu.ph>
2017-11-10 22:29:21 +0800

    Added Notifications Table

--- resources/views/navigation/navbar.blade.php
+++ resources/views/navigation/navbar.blade.php
@@ -2,6 +2,6 @@
 <div id="navigation">
     <a href="/" id="logo">TE<sup>3</sup>D<text id="logo2">Workshop</txt></a>
     <input type="text" name="search" placeholder="Search for works, authors, tags...">
-    <a href=""><img id="search_icon" src="{{URL::asset('/img/search_icon.png')}}" width="30"></a>
+    <a href=""><img id="search_icon" src="/storage/web_img/search_icon.png" width="30"></a>
     <a href="" class="navtxt">Sign In</a>
 </div>
--- resources/views/navigation/navbar_guest.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- NAVIGATION BAR -->
-<div id="navigation">
-    <a href="/" id="logo">TE<sup>3</sup>D<text id="logo2">Workshop</txt></a>
-    <input type="text" name="search" placeholder="Search for works, authors, tags...">
-    <a href=""><img id="search_icon" src="{{URL::asset('/img/search_icon.png')}}" width="30"></a>
-    <a href="" class="navtxt">Sign In</a>
-</div>
